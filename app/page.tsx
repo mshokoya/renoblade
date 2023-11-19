ba5ac2db2e1c8d5e1d149e6f7a2a5683664793d7
msho <mayo_s@hotmail.co.uk>
2023-11-19 14:44:12 +0000

    removed sync script

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
         YES WE WORK <span className='text-red-500'>COMPLETELY FOR FREE !!!</span>
       </div>
       <div className='mx-auto basis-auto'>
-      <div className='relative md:p-[20%] p-[40%]'><iframe id='vid' src="https://player.vimeo.com/video/886109081?badge=0&amp;autopause=0&amp;quality_selector=1&amp;player_id=0&amp;app_id=58479" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" style={{position:"absolute",top:0,left:0,width:"100%",height:"100%"}} title="vsl"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+      <div className='relative md:p-[20%] p-[40%]'><iframe id='vid' src="https://player.vimeo.com/video/886109081?badge=0&amp;autopause=0&amp;quality_selector=1&amp;player_id=0&amp;app_id=58479" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" style={{position:"absolute",top:0,left:0,width:"100%",height:"100%"}} title="vsl"></iframe></div>
         {/* <iframe width="560" height="315" src="https://youtu.be/embed/vobW_6dhESA&amp;controls=0&amp;start=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe> */}
       </div>
       {
